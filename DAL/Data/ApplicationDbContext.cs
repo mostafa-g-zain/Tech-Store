80bc874166dbc5a04e3c5f436ddd25cd010ae145
Mostafa Zain <mostafagamal201717@gmail.com>
2026-01-06 04:55:02 +0200

    Add project files.

---
+++ DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+
+    }
+}
